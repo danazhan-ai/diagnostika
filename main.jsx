05aff0f33146dd6b064d1e53a2fee497e91b92b0
danazhan-ai <dana.zhanabergenova@gmail.com>
2026-05-11 23:43:09 +0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,17 +1,9 @@
-{\rtf1\ansi\ansicpg1251\cocoartf2869
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab560
-\pard\pardeftab560\slleading20\partightenfactor0
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-\f0\fs26 \cf0 import React from 'react'\
-import \{ createRoot \} from 'react-dom/client'\
-import App from './App.jsx'\
-\
-createRoot(document.getElementById('root')).render(\
-  <React.StrictMode>\
-    <App />\
-  </React.StrictMode>\
-)}
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
